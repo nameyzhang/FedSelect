987d1e50590da65f0fc4d086829ff0314d4ff0d3
nameyzhang <90398149+nameyzhang@users.noreply.github.com>
2024-11-05 11:00:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main(args, logger):
     torch.cuda.manual_seed_all(args.seed)
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
+    random.seed(args.seed)
 
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
